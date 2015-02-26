324de4516381c6c110c36a39fe5ae33bba17cd78
Andreas Kvistad <ahk9339@gmail.com>
2015-02-26 10:48:43 +0100

    Fiksa main

--- Prosjekt/src/klient/Main.java
+++ Prosjekt/src/klient/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/ny_avtale.FXML"));
+			Parent root = FXMLLoader.load(getClass().getResource("/klient/ny_avtale.FXML"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
