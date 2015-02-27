1e6b7b14d0f75b463fbcd83dfd19d830c16ddb6a
My <My@78.91.27.41>
2015-02-27 12:06:24 +0100

    Nå kan man trykke på "Opprett bruker" knappen og bytte screens! Yay

--- Prosjekt/src/klient/Main.java
+++ Prosjekt/src/klient/Main.java
@@ -1,24 +1,47 @@
 package klient;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/klient/ny_avtale.FXML"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage stage) throws Exception{
+			stage.setTitle("Kalender Viewer");
+			stage.setScene(createScene ( loadMainPane()));
+			stage.show();
+//			Parent root = FXMLLoader.load(getClass().getResource("../klient/Startside_innlogging.FXML"));
+//			Scene scene = new Scene(root,1000, 700);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+	}
+	
+	private Pane loadMainPane() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		
+		Pane mainPane = (Pane) loader.load(
+				getClass().getResourceAsStream(
+						ScreenNavigator.MAIN
+						));
+		MainController mainController = loader.getController();
+		
+		ScreenNavigator.setMainController(mainController);
+		ScreenNavigator.loadScreen(ScreenNavigator.INNLOGGING);
+		
+		return mainPane;
+	}
+	
+	private Scene createScene(Pane mainPane){
+		Scene scene = new Scene(mainPane);
+		scene.getStylesheets().setAll(getClass().getResource("application.css").toExternalForm());
+		
+		return scene;
 	}
 	
 	public static void main(String[] args) {
---
+++ Prosjekt/src/klient/OpprettingController.java
@@ -0,0 +1,72 @@
+package klient;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class OpprettingController {
+
+    @FXML private TextField fornavn, etternavn, epost;
+    @FXML private Label fornavn_lbl, etternavn_lbl, epost_lbl,passord_lbl, passord2_lbl;
+    @FXML private PasswordField passord, passord2;
+
+    @FXML private Button lagre;
+
+    //Gå tilbake til Innloggingsiden
+    @FXML
+    void previousPane(ActionEvent event) {
+        ScreenNavigator.loadScreen(ScreenNavigator.INNLOGGING);
+    }
+ 
+    //Validere og sjekke om navn er gyldig, hver gang man trykker på en key
+   
+ /**   
+    @FXML
+    private void nameCheck(KeyEvent event) {
+    	if (isValidName()){
+    		
+    	}
+    }
+  
+    @FXML//Validere og sjekke om epost er gyldig, hver gang man trykker på en key
+    void 948c8c(ActionEvent event) {
+
+    }
+
+    @FXML //Validere og sjekke om passord er likt med passord 2 
+    void 948c8c(ActionEvent event) {
+
+    }
+
+    @FXML
+    void 9a9797(ActionEvent event) {
+
+    }
+    
+    public boolean isValidName(){
+    	String regex = "^[a-zA-Z]+$";
+    	if (fornavn.getText().matches(regex)){
+    		fornavn.setStyle("-fx-text-box-border : red");
+    		fornavn_lbl.setText("*Kan kun bestå av bokstaver.");
+    	}else if(etternavn.getText().matches(regex)){
+    		etternavn.setStyle("-fx-text-box-border : red");
+    		etternavn.setText("*Kan kun bestå av bokstaver.");
+    	}else if (etternavn.getText().isEmpty()){
+    		etternavn.setStyle("-fx-text-box-border : red");
+    		etternavn_lbl.setText("* Feltet må fylles.");
+    	}else if(fornavn.getText().isEmpty()){
+    		fornavn.setStyle("-fx-text-box-border : red");
+    		fornavn_lbl.setText("* Feltet må fylles.");
+    	}else{
+    		fornavn.setStyle("-fx-text-box-border : white");
+    		fornavn_lbl.setText("");
+    		etternavn.setStyle("-fx-text-box-border : white");
+    		etternavn_lbl.setText("");
+    	}
+    	
+    }*/
+
+}
