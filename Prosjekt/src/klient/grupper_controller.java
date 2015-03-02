e61d4e7bba22bf0d58297dfa4d6e9d72d816c5f0
lsnellingen <larssnellingen@hotmail.com>
2015-03-02 11:20:03 +0100

    Grupper fxml og controller

---
+++ Prosjekt/src/klient/grupper_controller.java
@@ -0,0 +1,41 @@
+package klient;
+
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class grupper_controller {
+	
+	private ArrayList<Bruker> medlemmer;
+	
+	public void grupper_controller(){
+		medlemmer = new ArrayList<Bruker>();
+	}
+
+	private Scene scene;
+    public void setScene(Scene scene) { this.scene = scene; }
+    
+    @FXML
+    TextField gruppenavn = new TextField();;
+    @FXML
+    TextField brukersøk = new TextField();
+    @FXML
+    ListView brukerliste = new ListView();
+    @FXML
+    ListView gruppemedlemmer_liste = new ListView();
+    @FXML
+    Slider gruppemedlemmer_slider = new Slider();
+    @FXML
+    TextField antall_gruppemedlemmer = new TextField();
+    @FXML
+    Button forkast_gruppe_knapp = new Button();
+    @FXML
+    Button lagre_gruppe_knapp = new Button();
+	
+}
