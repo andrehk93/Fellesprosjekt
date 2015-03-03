22fb34c562f5a49247b08581089387cef397d710
Martin Liahagen <liahagan@gmail.com>
2015-03-03 12:30:26 +0100

    labels on init

--- Prosjekt/src/klient/KalenderController.java
+++ Prosjekt/src/klient/KalenderController.java
@@ -1,11 +1,28 @@
 package klient;
 
+import java.util.ArrayList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class KalenderController {
 	
+	private ArrayList<Label> labels;
+	@FXML private GridPane ruter;
 	
+	public void initialize(){
+		loadLabels();
+	}
+	
+	private void loadLabels(){
+		for(int i=0;i<7;i++){
+			for(int j=0;j<8;j++){
+				ruter.add(new Label(i+" "+j), i, j);
+			}
+		}
+	}
 	
 	@FXML
 	private void nextPaneDayView(ActionEvent event) {
