e20c11ea963d49602b823c07453a569367c87917
My <My@78.91.25.154>
2015-03-04 13:10:47 +0100

    Lagde LogOut_button på KAlendervisnings-FXML filene. LogOut ActionsEvents også. Du havner tilbake på innloggingssiden ved klikk.

--- Prosjekt/src/klient/KalenderController.java
+++ Prosjekt/src/klient/KalenderController.java
@@ -13,8 +13,23 @@ public class KalenderController {
 	}
 	
 	@FXML
+	private void nextPaneWeekView(ActionEvent event) {
+		ScreenNavigator.loadScreen(ScreenNavigator.UKESVISNING);
+	}
+	
+	@FXML
 	private void nextPaneMakeAppointment(ActionEvent event) {
 		ScreenNavigator.loadScreen(ScreenNavigator.AVTALE);
+	}
+	
+	@FXML
+	private void nextPaneEditGroups(ActionEvent event) {
+		ScreenNavigator.loadScreen(ScreenNavigator.GRUPPER);
+	}
+	
+	@FXML
+	private void nextPaneLogOut(ActionEvent event) {
+		ScreenNavigator.loadScreen(ScreenNavigator.INNLOGGING);
 	}
 
 
