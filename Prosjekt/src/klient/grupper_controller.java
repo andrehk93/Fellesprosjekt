e0ec74b98d9021223ae973242c43e0ea04249b46
lsnellingen <larssnellingen@hotmail.com>
2015-03-05 14:11:18 +0100

    Nå kan man velge ting i brukerliste og legge til i grupper

--- Prosjekt/src/klient/grupper_controller.java
+++ Prosjekt/src/klient/grupper_controller.java
@@ -1,12 +1,16 @@
 package klient;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -18,13 +22,13 @@ import javafx.scene.input.KeyEvent;
 
 public class grupper_controller {
 	
-	private ArrayList<Bruker> medlemmer;
+	private ArrayList<String> medlemmer;					// Må endres til bruker når jeg før inn innhold i listen brukere
 	private ArrayList<Bruker> brukere;
 	private String søk;
+	private ArrayList<String> splitList;
 	private ArrayList<String> søk_liste;
 	private ArrayList<String> søk_liste2;
 	private ArrayList<String> testliste;
-	private ArrayList<String> søkBruker_liste;
 	private Scene scene;
     public void setScene(Scene scene) { this.scene = scene; }
     
@@ -36,29 +40,33 @@ public class grupper_controller {
     ListView<String> brukerliste = new ListView<>();
     @FXML
 	ListView<String> gruppemedlemmer_liste = new ListView<>();
-
     @FXML
     Slider gruppemedlemmer_slider = new Slider();
     @FXML
     TextField antall_gruppemedlemmer = new TextField();
     @FXML
+    Button legg_til_knapp = new Button();
+    @FXML
+    Button fjern_knapp = new Button();
+    @FXML
     Button forkast_gruppe_knapp = new Button();
     @FXML
     Button lagre_gruppe_knapp = new Button();
     
-    
-	
-	public grupper_controller(){
-		medlemmer = new ArrayList<Bruker>();
+    public void initialize() {
+    	addListner();
+		medlemmer = new ArrayList<String>();			// Må endres til bruker når jeg før inn innhold i listen brukere
 		brukere = new ArrayList<Bruker>();
+		testliste = new ArrayList<String>();			// Må endre alle testliste til brukere når jeg får listen fra databasen
 		testliste = new ArrayList<String>();
-		søkBruker_liste = new ArrayList<String>();
+		splitList = new ArrayList<String>();
 		søk_liste = new ArrayList<String>();
 		søk_liste2 = new ArrayList<String>();
 		testlisteAdd();
 		brukerliste(testliste);
-		
-	}
+		gruppemedlemmer_liste(medlemmer);
+    }
+	
 	
 	public void testlisteAdd(){
 		testliste.add("Andreas");
@@ -67,43 +75,117 @@ public class grupper_controller {
 		testliste.add("Martin");
 		testliste.add("My");
 	}
-	
-	public void brukerliste(ArrayList SøkBrukere){
+//-------------------------------------------------------------------------------------------------------------------------------------	
+	public void brukerliste(ArrayList<String> SøkBrukere){
     	ObservableList<String> liste_brukere = FXCollections.observableList(SøkBrukere);
 		brukerliste.setItems(liste_brukere);
 	}
+	
+	public void gruppemedlemmer_liste(ArrayList<String> GruppeMedlemmer){
+    	ObservableList<String> liste_gruppeMedlemmer = FXCollections.observableList(GruppeMedlemmer);
+		gruppemedlemmer_liste.setItems(liste_gruppeMedlemmer);
+	}
+	
+	public void leggeTilIGruppe(){
+		brukerliste.getSelectionModel().getSelectedItem();
+		medlemmer.add(brukerliste.getSelectionModel().getSelectedItem());
+		testliste.remove(brukerliste.getSelectionModel().getSelectedItem());
+	}
+	
+	public void addListner(){
+		ChangeListener<String> søker= new ChangeListener<String>() {
+
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				søk = newValue;
+				if (testliste.isEmpty() && søk.length()<1){
+		    		brukerliste(testliste);
+		    	} 
+		    	else{
+		            brukerliste(testliste);
+		    	}
+				if (!medlemmer.isEmpty()){
+					gruppemedlemmer_liste(medlemmer);
+				}
+				
+		    	testliste.clear();
+		    	if (søk != null){
+		    		for (int i = 0; i < testliste.size(); i++){
+		    			søk_liste2 = splitList(testliste.get(i));
+	    				System.out.println(søk_liste2);
+				    	søk_liste = splitList(søk);
+	    				System.out.println(søk_liste);
+	    				if (søk.length() < testliste.get(i).length()){
+		    				for (int j = 0; j < søk.length(); j++){
+		    					if (søk_liste.get(j).equals(søk_liste2.get(j))){
+		    						if (!testliste.contains(testliste.get(i))){
+		    							testliste.add(testliste.get(i));
+		    						}
+		    					}
+		    				}
+	    				}
+		    		}
+		    	}
+
+			}
+		};
+		brukersøk.textProperty().addListener(søker);
+	}
     
+	
+	public ArrayList<String> splitList(String e){
+		splitList.clear();
+		for (int i = 0; i < e.length(); i++){
+			splitList.add(Character.toString(e.charAt(i)));
+		}
+		return splitList;
+	}
+	
     @FXML
     public void handleGruppesøk(KeyEvent event) throws IOException {
-    	søkBruker_liste.clear();
-    	søk = brukersøk.getText();
     	
-    	for (int i = 0; i < testliste.size(); i++){
-    		søkBruker_liste.add(testliste.get(i));
-    	}
-    	
-/*		søk_liste = søk.split("");
-    	for (int i = 0; i < testliste.size(); i++){
-    		if (søk.length() < testliste.get(i).length()){
-    			for (int j = 0; j < søk.length(); j++){
-    				søk_liste2 = testliste.get(i).split("");
-    				if (søk_liste[j] == søk_liste2[j]){
-    					søkBruker_liste.add(testliste.get(i));
-    				}
-    			}
-    		}
-    	}
-    	System.out.println(søk_liste[1]);
-    	System.out.println(søk_liste2[1]);*/
-    	
-    	System.out.println(søk);
-    	brukerliste(søkBruker_liste);
     }
+    
+    @FXML
+    private void handleLeggTil(ActionEvent event) throws IOException, NoSuchAlgorithmException {
+    	if (brukerliste.getSelectionModel().getSelectedItem() != null){
+    		brukerliste.getSelectionModel().getSelectedItem();
+			medlemmer.add(brukerliste.getSelectionModel().getSelectedItem());
+			testliste.remove(brukerliste.getSelectionModel().getSelectedItem());
+    	}
+		if (testliste.isEmpty() && søk == null){
+    		brukerliste(testliste);
+    	} 
+    	else{
+            brukerliste(testliste);
+    	}
+		gruppemedlemmer_liste(medlemmer);
+
+	}
+    
+    @FXML
+    private void handleFjern(ActionEvent event) throws IOException, NoSuchAlgorithmException {
+		if (gruppemedlemmer_liste.getSelectionModel().getSelectedItem() != null){
+	    	gruppemedlemmer_liste.getSelectionModel().getSelectedItem();
+			testliste.add(gruppemedlemmer_liste.getSelectionModel().getSelectedItem());
+			medlemmer.remove(gruppemedlemmer_liste.getSelectionModel().getSelectedItem());
+		}
+		
+		if (testliste.isEmpty() && søk == null){
+    		brukerliste(testliste);
+    	} 
+    	else{
+            brukerliste(testliste);
+    	}
+		gruppemedlemmer_liste(medlemmer);
+		
+	}
     
     
     public void getUsers() throws IOException{
     	brukere = Klienten.getAllUserDetails();
     	brukerliste.setItems((ObservableList) brukere);
+    	brukerliste(testliste);
     }
     
     
